b2db74370c5ca9bc83d4c581240c35da6f37e790
Rasyid <rasyidridl298@gmail.com>
2020-10-26 09:33:02 +0700

    Initial Commit

---
+++ Lat Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lat Auto Layout
+//
+//  Created by Rasyid Ridla on 26/10/20.
+//  Copyright © 2020 Rr Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
