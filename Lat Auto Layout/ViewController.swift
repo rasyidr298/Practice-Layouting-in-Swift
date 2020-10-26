29ea5b0aa9ad52f90276b916ad4b402bd6d59d7b
Rasyid <rasyidridl298@gmail.com>
2020-10-26 14:01:18 +0700

    layouting profile no responsive

--- Lat Auto Layout/ViewController.swift
+++ Lat Auto Layout/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        @IBOutlet weak var topView: UIView!
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            
+            topView.roundedBottom()
+        }
+
+
     }
 
-
-}
-
+    extension UIView{
+        func roundedBottom(){
+            let maskPath1 = UIBezierPath(roundedRect: bounds,
+                                         byRoundingCorners: [.bottomRight , .bottomLeft],
+                                         cornerRadii: CGSize(width: 15, height: 15))
+            let maskLayer1 = CAShapeLayer()
+            maskLayer1.frame = bounds
+            maskLayer1.path = maskPath1.cgPath
+            layer.mask = maskLayer1
+        }
+    }
