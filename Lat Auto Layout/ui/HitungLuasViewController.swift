9f1d9593dd894a8a554fc243c0e74ddd8eb69a9d
Rasyid <rasyidridl298@gmail.com>
2020-11-04 16:25:16 +0700

    - add View HomePage - add Navigation Controller - add View HitungLuas

---
+++ Lat Auto Layout/ui/HitungLuasViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class HitungLuasViewController: UIViewController {
+    
+    @IBOutlet weak var tfHasil: UILabel!
+    @IBOutlet weak var lbPanjang: UITextField!
+    @IBOutlet weak var lbTinggi: UITextField!
+    @IBOutlet weak var btHitung: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
