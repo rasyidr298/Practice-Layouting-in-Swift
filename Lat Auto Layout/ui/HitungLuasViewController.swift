bf949e7f39c3a3a41f9284bd73bd4052f3ba5203
Rasyid <rasyidridl298@gmail.com>
2020-11-05 10:32:18 +0700

    added area calculation feature and fix auto layout

--- Lat Auto Layout/ui/HitungLuasViewController.swift
+++ Lat Auto Layout/ui/HitungLuasViewController.swift
@@ -5,7 +5,22 @@ class HitungLuasViewController: UIViewController {
     @IBOutlet weak var tfHasil: UILabel!
     @IBOutlet weak var lbPanjang: UITextField!
     @IBOutlet weak var lbTinggi: UITextField!
-    @IBOutlet weak var btHitung: UIButton!
+    
+    @IBAction func btnHitung(_ sender: UIButton) {
+        let panjang = lbPanjang.text!
+        let tinggi = lbTinggi.text!
+        
+        //konversi ke float
+        let floatPanjang = Float(panjang)!
+        let floatTinggi = Float(tinggi)!
+        
+        //rumus
+        let hasil = 0.5 * floatPanjang * floatTinggi
+        
+        //tampilkan dan konversi ke String
+        tfHasil.text = String(hasil)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
